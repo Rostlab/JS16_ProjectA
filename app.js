7f4fa41a8a242e7ff254a2804403a2c57a9dd19b
MiLe <michael.legenc@tum.de>
2016-02-21 18:54:21 +0100

    Hello world

--- app.js
+++ app.js
@@ -4,7 +4,3 @@ var app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
