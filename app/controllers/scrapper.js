4f29a08dfc5402584f42b62a12fb86613b6c14cb
Theodor Cheslerean Boghiu <theo.cheslerean@tum.de>
2016-02-23 23:34:55 +0100

    Scrapper 2. Version

--- app/controllers/scrapper.js
+++ app/controllers/scrapper.js
@@ -4,15 +4,15 @@ module.exports = {
 		var request = require('request');
 		
 		url = 'http://awoiaf.westeros.org/index.php/Houses_of_Westeros';
-		
 		request(url, function(error, response, html) {			
 			var body = cheerio.load(html);
-			body('.navbox-list').filter(function() {
+			var arr = body('.navbox-list').filter(function() {
 				var data = body(this);
 				/*
 					For testing purposes
 					Write to console all houses names
 				*/
+				var arr = data.children().text().split("·");
 				/*
 				for(i = 0; i < arrayOfChildren.length; i++) {
 					var child = arrayOfChildren[i];
@@ -20,6 +20,20 @@ module.exports = {
 				}
 				*/
 			})
-		});  
-    }
+		});	
+    },
+	
+	scrapWebPageNew: function (req, res) {
+		var MediaWiki = require("mediawiki");
+		var bot = new MediaWiki.Bot({
+			endpoint: "http://awoiaf.westeros.org/api.php"
+		});
+		//var url = http://awoiaf.westeros.org/api.php?action=query&titles=Houses%20Of%20Westeros&list=search&srsearch=house&format=xml&sroffset=10";
+		bot.get({action: "query", titles: "Houses%20Of%20Westeros", list: "search", srsearch: "house", format: "xml", sroffset: "0"}).complete(function(response) {
+			console.log(response.text);
+		}).error(function (err) {
+			console.log(err.toString());
+		});		
+	}
+	
 };
