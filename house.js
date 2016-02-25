3038077d3dadd2f68c1ca235718e0acdf6e91390
Sohel Mahmud <sohel.digitalhive@gmail.com>
2016-02-25 02:00:54 +0100

    Delete house.js

--- house.js
+++
@@ -1,9 +0,0 @@
-var mongoose     = require('mongoose');
-var Schema       = mongoose.Schema;
-
-var HouseSchema   = new Schema({
-    name: String,
-    character: [{type: Schema.Types.ObjectId, ref: "Character"}]
-});
-
-module.exports = mongoose.model('House', HouseSchema);
