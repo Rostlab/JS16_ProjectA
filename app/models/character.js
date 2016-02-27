dfaa4139e5384261653e8b3b975b5a190c2926f9
MiLe <michael.legenc@tum.de>
2016-02-28 00:13:57 +0100

    Fixed types, name is now required.

--- app/models/character.js
+++ app/models/character.js
@@ -4,35 +4,16 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var CharacterSchema = new Schema({
-    _id: ObjectId,
-    name: String,                                                         // Rhaegar
+    name: {type: String, required: true},                                // Rhaegar
     title: String,                                                        // Prince of Dragonstone Ser
-    gender: String,                                                       // Male 
+    male: boolean,                                                       // Male
     culture: String,                                                      // Valyrian
-    age: {type: Number, min: 8, max: 100},                              // ??
-    born: Date,                                                           // 259 AC 
-    died: Date,  	                                                     // 283 AC
+    age: {type: Number, min: 1, max: 200},                              // ??
+    born: String,                                                        // 259 AC
+    died: String,  	                                                     // 283 AC
 
-    /*
-     brothersSisters: {
-     characters:[{
-     _id: { type: String, ref: 'Character' }
-     }]
-     }, //no sister
-     mother: {
-     character:{
-     _id: { type: String, ref: 'Character' }
-     }
-     },		// was the eldest son of King Aerys II Targaryen
-     father: {
-     character:{
-     _id: { type: String, ref: 'Character' }
-     }
-     },		// his sister-wife, Queen Rhaella.
-     */
-
-
-    brothersSisters: [{type: Schema.Types.ObjectId, ref: 'Character'}],
+    brothers: [{type: Schema.Types.ObjectId, ref: 'Character'}],
+    sisters: [{type: Schema.Types.ObjectId, ref: 'Character'}],
     mother: {type: Schema.Types.ObjectId, ref: 'Character'},
     father: {type: Schema.Types.ObjectId, ref: 'Character'},
     children: [{type: Schema.Types.ObjectId, ref: 'Character'}],
@@ -43,12 +24,12 @@ var CharacterSchema = new Schema({
     placeOfBirth: String,                                                 // Summerhall
     placeOfDeath: String,                                                 // Trident
     house: {type: Schema.Types.ObjectId, ref: "House"},                   // House Targaryen
-    spouse: String,                                                       // Princess Elia Martell
-    skill: [{type: Schema.Types.ObjectId, ref: "Skill"}],               // one to many - talented musician and skilled knight
+    spouse: [{type: Schema.Types.ObjectId, ref: 'Character'}],            // Princess Elia Martell
+    skill: [{type: Schema.Types.ObjectId, ref: "Skill"}],                // one to many - talented musician and skilled knight
     episode: [{type: Schema.types.ObjectId, ref: 'Episode'}],    // one character appears in different episodes
-    event_name: String,                                                   // A game of thrones, A clash with kings
-    created_at: {type: Date, default: Date.now},
-    updated_at: {type: Date, default: Date.now}
+    eventName: String,                                                   // A game of thrones, A clash with kings
+    createdAt: {type: Date, default: Date.now},
+    updatedAt: {type: Date, default: Date.now}
 
 });
 
