ec09ab6daded7c68fd0f1681df635bc78b45af2d
MiLe <michael.legenc@tum.de>
2016-02-27 19:29:55 +0100

    Removed old comments.

--- app/models/house.js
+++ app/models/house.js
@@ -3,16 +3,12 @@ var Schema       = mongoose.Schema;
 
 var HouseSchema   = new Schema({
     name: String,                                                         // Targaryen
-    type: {type: Schema.Types.ObjectId, ref: "HouseTypes"},                                                         // 0 = Current Great house
-                                                                          // 1 = Exiled Great house
-                                                                          // 2 = Extinct Great house
+    type: {type: Schema.Types.ObjectId, ref: "HouseTypes"},               // Current, exiled or extinct Great house
     coatOfArms: String,                                                   // Sable, a dragon thrice-headed gules
     words: String,                                                        // Fire and Blood
-    currentLord: {type: Schema.Types.ObjectId, ref: "Character"},                 // Queen Daenerys Targaryen
-                          // What means current? After season/book?
-    overlord: {type: Schema.Types.ObjectId, ref: "Character"},                                                     // None
+    currentLord: {type: Schema.Types.ObjectId, ref: "Character"},         // Queen Daenerys Targaryen
+    overlord: {type: Schema.Types.ObjectId, ref: "Character"},            // None
     title: [String],                                                      // King of the Andals, the Rhoynar, and the First Men,Lord of the Seven Kingdoms
-                                                                          // Prince of Dragonstone, Prince of Summerhall
     region: String,                                                       // Crownlands (formerly Valyria)
     cadetBranch: String,                                                  // House Blackfyre is the cadet branch of House Targaryen
     ancestralWeapon: [String],                                            // Blackfyre and Dark Sister
