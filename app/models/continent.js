60558163eb60e7a1f0ce48ba159055836e387e14
MiLe <michael.legenc@tum.de>
2016-02-28 23:18:28 +0100

    name is now required.

--- app/models/continent.js
+++ app/models/continent.js
@@ -2,7 +2,7 @@ var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
 var ContinentSchema   = new Schema({
-    name: String,
+    name: {type: String, required: true},
 	neighbors: [{type: Schema.Types.ObjectId, ref: "Region"}]
 });
 
