1cff3953942a81b1c0c563f8e0651e3cb0d81a8e
MiLe <michael.legenc@tum.de>
2016-02-28 21:50:53 +0100

    _id removed

--- app/models/battle.js
+++ app/models/battle.js
@@ -2,9 +2,8 @@ var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
 var BattleSchema   = new Schema({
-	_id: Schema.Types.ObjectId,
-    name: String,                                                         // battle's name
-    date: Date,                                                        // > 126 BC
+    name: {type: String, required: true},                                                         // battle's name
+    date: String,                                                        // > 126 BC
     characters: [{type: Schema.Types.ObjectId, ref: 'Character'}],          // Characters fought in this battle
 	charactersDied: [{type: Schema.Types.ObjectId, ref: 'Character'}],          // Characters died in this battle
 	event: {type: Schema.Types.ObjectId, ref: "Event"}
