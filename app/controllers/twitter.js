280b9c46145c7f72789c7cf26e9cd2b2f3e40040
Togi Dashnyam <togi.berlin@gmail.com>
2016-02-28 13:33:40 +0100

    Edited error handling

--- app/controllers/twitter.js
+++ app/controllers/twitter.js
@@ -17,7 +17,7 @@ module.exports = {
                 res.status(200).json(tweet)
             });
             stream.on('error', function (error) {
-                throw error;
+                res.status(400).json({ message: 'Error', error: error });
             });
         });
     }
