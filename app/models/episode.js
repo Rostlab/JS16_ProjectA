129a2273eb1de0a2ead046c6b162efd127bf5504
MiLe <michael.legenc@tum.de>
2016-02-28 20:38:55 +0100

    [Issue #24]

--- app/models/episode.js
+++ app/models/episode.js
@@ -4,14 +4,14 @@ var Schema = mongoose.Schema;
 var EpisodeSchema = new Schema({
     name: String,
     characters: [{type: Schema.Types.ObjectId, ref: 'Character'}],
-    start_time: {type: Date, default: Date.now},
-    episode_length: {type: Number, min: 60, max: 70},        // maximum 70 min, minimum 60 min duration as the episodes differ in duration
+    startTime: {type: Date, default: Date.now},
+    episodeLength: {type: Number, min: 60, max: 70},        // maximum 70 min, minimum 60 min duration as the episodes differ in duration
 
     predecessor: {type: Schema.Types.ObjectId, ref: 'Episode'},
     successor: {type: Schema.Types.ObjectId, ref: 'Episode'},
 
-    created_at: {type: Date, default: Date.now},
-    updated_at: {type: Date, default: Date.now}
+    createdAt: {type: Date, default: Date.now},
+    updatedAt: {type: Date, default: Date.now}
 });
 
 module.exports = mongoose.model('Episode', EpisodeSchema);
