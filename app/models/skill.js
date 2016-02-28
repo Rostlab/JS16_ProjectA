b591534573ba842d6f2960bc37909dcecadd49f6
MiLe <michael.legenc@tum.de>
2016-02-28 21:49:16 +0100

    _id removed

--- app/models/skill.js
+++ app/models/skill.js
@@ -2,7 +2,6 @@ var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
 var SkillSchema   = new Schema({
-	_id: ObjectId,
     name: String,
     character: [{type: Schema.Types.ObjectId, ref: "Character"}]   //  a particular skill can be obtained by many characters
 });
