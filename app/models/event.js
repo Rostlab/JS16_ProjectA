d7ff391b0f0e5fd30e347164f36eff061ec4bc95
MiLe <michael.legenc@tum.de>
2016-02-28 21:46:07 +0100

    [Issue #24]

--- app/models/event.js
+++ app/models/event.js
@@ -2,21 +2,13 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var EventSchema = new Schema({
-
-    _id: ObjectId,
     name: String,
     befAC: Boolean,
     date: Date,
     age: {type: Schema.types.ObjectId, ref: 'Age'},
-    //characters: [{type: Schema.types.ObjectId, ref: 'Character'}],
-	//battles: [{type: Schema.types.ObjectId, ref: 'Battle'}],
-    
-    
-    //predecessor: {type: Schema.types.ObjectId, ref: 'Episode'},
-    //successor: {type: Schema.types.ObjectId, ref: 'Episode'},
 
-    created_at: {type: Date, default: Date.now},
-    updated_at: {type: Date, default: Date.now},
+    createdAt: {type: Date, default: Date.now},
+    updatedAt: {type: Date, default: Date.now},
 });
 
 module.exports = mongoose.model('Event', EventSchema);
