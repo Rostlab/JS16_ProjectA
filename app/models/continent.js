df1770e9fbe17aa1f373cae08357dfd38830a65b
MiLe <michael.legenc@tum.de>
2016-02-28 23:17:26 +0100

    Removed _id

--- app/models/continent.js
+++ app/models/continent.js
@@ -2,7 +2,6 @@ var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
 var ContinentSchema   = new Schema({
-	_id: ObjectId,
     name: String,
 	neighbors: [{type: Schema.Types.ObjectId, ref: "Region"}]
 });
