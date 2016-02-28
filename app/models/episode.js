1cfa811430381f7a86fb0477a68db10c0f98a96c
MiLe <michael.legenc@tum.de>
2016-02-28 20:45:52 +0100

    [Issue #23]

--- app/models/episode.js
+++ app/models/episode.js
@@ -2,9 +2,12 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var EpisodeSchema = new Schema({
+    totalNr: Number, // episode nr. overall
+    nr: Number, // episode nr in season
+    season: Number,
     name: String,
     characters: [{type: Schema.Types.ObjectId, ref: 'Character'}],
-    startTime: {type: Date, default: Date.now},
+    airdate: Date,
     episodeLength: {type: Number, min: 60, max: 70},        // maximum 70 min, minimum 60 min duration as the episodes differ in duration
 
     predecessor: {type: Schema.Types.ObjectId, ref: 'Episode'},
