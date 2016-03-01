25fa0761f25a2f8ea052104e3c833d385cdd6a2e
Boris Idesman <boris.idesman@tum.de>
2016-03-01 17:13:10 +0100

    Sample data

---
+++ sample data/continents.js
@@ -0,0 +1,21 @@
+{
+    "name" : "Essos",
+    "neighbors" : [ 
+        "Westeros", 
+        "Sothoryos"
+    ]
+}
+
+{
+    "name" : "Westeros",
+    "neighbors" : [ 
+        "Essos"
+    ]
+}
+
+{
+    "name" : "Sothoryos",
+    "neighbors" : [ 
+        "Essos"
+    ]
+}
