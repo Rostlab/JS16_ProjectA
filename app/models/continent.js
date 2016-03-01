bc22575c5d7d1f735019b3237aad3843b36d078c
Boris Idesman <boris.idesman@tum.de>
2016-03-02 00:50:49 +0100

    Bug fix

--- app/models/continent.js
+++ app/models/continent.js
@@ -3,7 +3,7 @@ var Schema       = mongoose.Schema;
 
 var ContinentSchema   = new Schema({
     name: {type: String, required: true},
-	neighbors: [{type: Schema.Types.ObjectId, ref: "Region"}]
+	neighbors: [{type: Schema.Types.ObjectId, ref: "Continent"}]
 });
 
 module.exports = mongoose.model('Continent', ContinentSchema);
