590ba6594d21136da4350faed427d333c0dd874d
MiLe <michael.legenc@tum.de>
2016-03-01 22:23:18 +0100

    Make field to be required.

--- app/models/skill.js
+++ app/models/skill.js
@@ -2,7 +2,7 @@ var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
 var SkillSchema   = new Schema({
-    name: String,
+    name: {type: String, required: true},
     character: [{type: Schema.Types.ObjectId, ref: "Character"}]   //  a particular skill can be obtained by many characters
 });
 
