04909d893299d5094d0a3b134912e4138a7acbcc
Theodor Cheslerean Boghiu <theo.cheslerean@tum.de>
2016-03-10 14:48:55 +0100

    [Issue #46]: Event scraper done

--- app/controllers/scraper.js
+++ app/controllers/scraper.js
@@ -371,16 +371,133 @@ module.exports = {
 				ages[i]["startDate"] = start;
 				ages[i]["endDate"] = end;
 			}
-			
-
-			
-			
-			
 			callback(ages);
 		});
 	},
 	
-    getAllHistory: function (req, res) {
+	getAgesWithEvents : function(callback) {
+		var bot = require("nodemw");
+
+        var client = new bot({
+            server: "awoiaf.westeros.org",
+            path: "/api.php"
+        });
+        var params = {
+            action: "parse",
+            page: "Timeline_of_major_events",
+            format: "json"
+        };
+		var ages = [];
+		client.api.call(params, function(err, info, next, data) {
+			
+			var arr = data["parse"]["text"]["*"].match(/<span\sclass=\"tocnumber(.*?)>(.*?)<\/a>/g);
+			for(i = 1; i < arr.length-2; i++) {
+				tmp = arr[i].match(/\">(.*?)<\/span>/g);
+				ageName = tmp[1].substring(2, tmp[1].length-7);
+				var age = {};
+				age["name"] = ageName;
+				ages.push(age);
+			}
+			for(i = 0; i < ages.length; i++) {
+				console.log(ages[i]);
+				if(i > 0 && i < ages.length-1) {
+					ages[i]["predecessor"] = ages[i-1]["name"];
+					ages[i]["successor"] = ages[i+1]["name"];
+				}
+				if(i == 0) {
+					ages[i]["successor"] = ages[i+1]["name"];
+				}
+				if(i == ages.length-1) {
+					ages[i]["predecessor"] = ages[i-1]["name"];
+				}
+			}
+			var arr = data["parse"]["text"]["*"].match(/<table\sclass=\"wikitable\">((.|[\r\n])*?)<\/table>/g);
+			var start, end;
+			for(i = 0; i < arr.length; i++) {
+				var q = arr[i].match(/<tr>\n<td(.*?)>((.|[\r\n])*?)<\/td>/g);
+				if(q[0].indexOf("a>") != -1) {
+					if(q[0].indexOf("width") != -1) {
+						q[0] = q[0].match(/\<a(.*?)">((.|[\r\n])*?)<\/a>/g)[0];
+						q[0] = q[0].substring(2, q[0].length);
+					}
+					var tmp = q[0].match(/\">((.|[\r\n])*?)<\/a>/g);
+					start = tmp[0].substring(2, tmp[0].length-4);
+					
+				}
+				else {
+					var tmp = q[0].match(/>((.|[\r\n])*?)</g);
+					start = tmp[1].substring(1, tmp[1].length-2);
+				}
+				
+				if(q[q.length-1].indexOf("a>") != -1) {
+					if(q[q.length-1].indexOf("width") != -1) {
+						q[q.length-1] = q[q.length-1].match(/\<a(.*?)">((.|[\r\n])*?)<\/a>/g)[0];
+						q[q.length-1] = q[q.length-1].substring(2, q[q.length-1].length);
+					}
+					var tmp = q[q.length-1].match(/\">((.|[\r\n])*?)<\/a>/g);
+					end = tmp[0].substring(2, tmp[0].length-4);
+					
+				}
+				else {
+					var tmp = q[q.length-1].match(/>((.|[\r\n])*?)</g);
+					end = tmp[1].substring(1, tmp[1].length-2);
+				}
+				
+				ages[i]["startDate"] = start;
+				ages[i]["endDate"] = end;
+			}
+			for(i = 0; i < arr.length; i++) {
+				var lines = arr[i].match(/<tr>((.|[\r\n])*?)<\/tr>/g);
+				events = [];
+				for(j = 0; j < lines.length; j++) {
+					
+					var date;
+					var event = {};
+					var eventName;
+					
+					var nrOfCells = lines[j].match(/<td(.*?)>((.|[\r\n])*?)<\/td>/g); 
+					if(nrOfCells.length == 1) {
+						continue;
+					}
+					var title = lines[j].match(/<td(.*?)>((.|[\r\n])*?)<\/td>/g)[0]; 
+					if(title.indexOf("a>") != -1) {
+						if(title.indexOf("width") != -1 || title.indexOf("rowspan") != -1) {
+							title = title.match(/\<a(.*?)">((.|[\r\n])*?)<\/a>/g)[0];
+							title = title.substring(2, title.length);
+						}
+						var tmp = title.match(/\">((.|[\r\n])*?)<\/a>/g);
+						date = tmp[0].substring(2, tmp[0].length-4);
+					}
+					else {
+						var tmp = title.match(/>((.|[\r\n])*?)</g);
+						date = tmp[0].substring(1, tmp[0].length-2);
+					}
+					
+					//console.log(date);
+					
+					
+					var content = lines[j].match(/<b>(.*?)<\/b>/g);
+					if(content != null) {
+						eventName = content[0].replace(/<a\shref(.*?)>/g, "");
+						eventName = eventName.replace(/<\/a>/g, "");
+						eventName = eventName.replace(/<b>/g, "");
+						eventName = eventName.replace(/<\/b>/g, "");
+					}
+					else {
+						eventName = "Unnamed";
+					}
+					event["name"] = eventName;
+					event["date"] = date;
+					events.push(event);
+					
+				}
+				ages[i]["events"] = events;
+			}
+			callback(ages);
+		});
+	},
+	
+    getEvents: function (callback) {
 
         //Setup the mediawiki bot
         var bot = require("nodemw");
@@ -390,28 +507,24 @@ module.exports = {
             path: "/api.php"
         });
         var params = {
-			action: "query",
-			titles: "Timeline_of_major_events",
-			list: "search",
-			srsearch: "mw-headline",
-			sroffset: String(i)
+            action: "parse",
+            page: "Timeline_of_major_events",
+            format: "json"
         };
 		
-
-
-        history = [];
-
-        //Iterate through all histroy
-
-        console.log("Loading all history from the wiki. This might take a while");
-        client.api.call(params, function (err, info, next, data) {
-            for (i = 0; i < data[""][""].length; i++) {
-                characters.push(data[""][""][i]["*"]);
-            }
-            res.status(200).json(history);
-        });
-
-        res.status(400).json({message: 'Error', error: "something went wrong"});
+		var scraper = require("./scraper");
+		var events = [];
+		scraper.getAgesWithEvents(function(ages) {
+			for(i = 0; i < ages.length; i++) {
+				for(j = 0; j < ages[i]["events"].length; j++) {
+					var event = ages[i]["events"][j];
+					event["age"] = ages[i]["name"];
+					events.push(event);
+				}
+			}
+			callback(events);
+		});
+		
     },
     
     getCultures: function (callback) {
--- app/models/event.js
+++ app/models/event.js
@@ -4,7 +4,7 @@ var Schema = mongoose.Schema;
 var EventSchema = new Schema({
     name: {type: String, required: true, unique: true},
     date: Number,
-    age: {type: Schema.Types.ObjectId, ref: 'Age'},
+    age: {type: String, ref: 'Age'},
 
     createdAt: {type: Date, default: Date.now},
     updatedAt: {type: Date, default: Date.now},
