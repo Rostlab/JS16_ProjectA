345e63f66410e0922389baeb04e5f15b5efbc384
Kordian Bruck <k@bruck.me>
2016-03-14 23:58:30 +0100

    #17 improve ages store

--- app/stores/ages.js
+++ app/stores/ages.js
@@ -8,104 +8,101 @@ module.exports = {
         // check if POST data matches Schema
         for (var key in data) {
             if (data.hasOwnProperty(key) && !Age.schema.paths.hasOwnProperty(key)) {
-                callback(2,key);
+                callback(2, key);
                 return;
             }
-            else
-            {
+            else {
                 age[key] = data[key];
             }
         }
 
-        age.save(function(err) {
-            if (err){
-                callback(3,err);
+        age.save(function (err) {
+            if (err) {
+                callback(3, err);
             }
             else {
-                callback(1,age);
+                callback(1, age);
             }
         });
     },
 
-    get: function(data, callback) {
+    get: function (data, callback) {
+        var rangeQuery = function (data, key) {
+            var sub = data[key].substring(0, 2);
+            if (sub == '>=') {
+                data[key] = {$gte: data[key].substring(2)};
+            } else if (sub == '<=') {
+                data[key] = {$lte: data[key].substring(2)};
+            } else if (data[key].indexOf('>') > -1) {
+                data[key] = {$gt: data[key].replace('>', '')};
+            } else if (data[key].indexOf('<') > -1) {
+                data[key] = {$lt: data[key].replace('<', '')};
+            }
+
+        };
+
         // check if POST data matches Schema
         for (var key in data) {
             if (data.hasOwnProperty(key) && !Age.schema.paths.hasOwnProperty(key)) {
-                callback(2,key);
+                callback(2, key);
                 return;
             }
 
-            var rangeQuery = function(key,callback) {
-                var sub = data[key].substring(0,2);
-                if (sub == '>=') {
-                    callback({$gte: data[key].substring(2)});
-                }
-                else if (sub == '<=') {
-                    callback({$lte: data[key].substring(2)});
-                }
-                else if (data[key].indexOf('>') > -1) {
-                    callback({$gt: data[key].replace('>', '')});
-                }
-                else if (data[key].indexOf('<') > -1) {
-                    callback({$lt: data[key].replace('<', '')});
-                }
-
-            }
-
+            //Allow dates to be ranges
             if (key == 'startDate' || key == 'endDate') {
-                rangeQuery(key,function(callback){
-                    data[key] = callback;
-                });
+                rangeQuery(data, key);
             }
         }
 
-        Age.find(data, function(err,obj)
-        {
-            if(obj.length === 0)
-                callback(3,data);
-            else
+        Age.find(data, function (err, obj) {
+            if (obj.length === 0) {
+                callback(3, data);
+            } else {
                 callback(1, obj);
-        });
-    },
-
-    getByName: function(name, callback) {
-        this.get({'name':name},function(success,message){
-            if(success == 1) {
-                callback(success,message[0]);
-            }
-            else {
-                callback(success,message);
             }
         });
     },
 
-    getById: function(id, callback) {
-        this.get({'_id': id},function(success,message){
-            if(success == 1) {
-                callback(success,message[0]);
+    getByName: function (name, callback) {
+        this.get({'name': name}, function (success, message) {
+            if (success == 1) {
+                callback(success, message[0]);
             }
             else {
-                callback(success,message);
+                callback(success, message);
+            }
+        });
+    },
+
+    getById: function (id, callback) {
+        this.get({'_id': id}, function (success, message) {
+            if (success == 1) {
+                callback(success, message[0]);
+            }
+            else {
+                callback(success, message);
             }
         });
     },
 
     getAll: function (callback) {
         Age.find(function (err, ages) {
-            if (err)
-                callback(false,err);
-            else
-                callback(true,ages);
+            if (err) {
+                callback(false, err);
+            } else {
+                callback(true, ages);
+            }
         });
     },
 
     remove: function (id, callback) {
-        Age.remove({_id: id}, function(err, resp) {
+        Age.remove({_id: id}, function (err, resp) {
             // more than zero entries removed?
-            if (resp.result.n > 0)
+            if (resp.result.n > 0) {
                 callback(true);
-            else
+            } else {
                 callback(false);
+            }
         });
 
     },
@@ -114,25 +111,25 @@ module.exports = {
         // check if POST data matches Schema
         for (var key in data) {
             if (data.hasOwnProperty(key) && !Age.schema.paths.hasOwnProperty(key)) {
-                callback(4,key);
+                callback(4, key);
                 return;
             }
         }
 
-        this.getById(id,function(success, age) {
+        this.getById(id, function (success, age) {
             // Age exists
-            if(success == 1) {
+            if (success == 1) {
                 for (var key in data) {
                     if (data.hasOwnProperty(key)) {
                         Age[key] = data[key];
                     }
                 }
-                Age.save(function(err) {
-                    if (err){
-                        callback(3,err);
+                Age.save(function (err) {
+                    if (err) {
+                        callback(3, err);
                     }
                     else {
-                        callback(1,Age);
+                        callback(1, Age);
                     }
                 });
             }
