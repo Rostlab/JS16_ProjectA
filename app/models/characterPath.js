5dd4fddc2cbec2627cd607c5f94dc0e7b279884e
MiLe <michael.legenc@tum.de>
2016-03-18 20:41:20 +0100

    fix travis

--- app/models/characterPath.js
+++ app/models/characterPath.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose');
 var Schema   = mongoose.Schema;
 
 var CharacterPathSchema = new Schema({
-    character : {type: Schema.Types.ObjectId, ref: “Character”},
+    character : {type: Schema.Types.ObjectId, ref: 'Character'},
     path: String	//Required as store for json object, containing the path 
 });
 
-module.exports = mongoose.model(‘CharacterPath’, CharacterPathSchema);
+module.exports = mongoose.model('CharacterPath', CharacterPathSchema);
