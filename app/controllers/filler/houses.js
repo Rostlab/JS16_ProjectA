9faecae5e4dbaf622b156db05821e773ccd4b594
MiLe <michael.legenc@tum.de>
2016-03-18 18:34:19 +0100

    #81 filler policies for houses

--- app/controllers/filler/houses.js
+++ app/controllers/filler/houses.js
@@ -3,14 +3,13 @@ var House = require(__appbase + 'models/house');
 var Houses = require(__appbase + 'stores/houses');
 var jsonfile = require('jsonfile');
 var async = require('async');
-var ttl = require(__appbase + '../cfg/config.json');
+var cfg = require(__appbase + '../cfg/config.json');
 
 module.exports = {
     fill: function(req, res) {
         console.log('Filling started.');
 
-        var afterInsertion = function()
-        {
+        var afterInsertion = function() {
             console.log('Filling done =).');
         };
 
@@ -28,7 +27,7 @@ module.exports = {
         jsonfile.readFile(file, function(err, obj) {
             if(obj !== undefined) {
                 var cacheAge = ((new Date()) - new Date(obj.createdAt));
-                if(cacheAge > ttl.TTLWikiCache) {
+                if(cacheAge > cfg.TTLWikiCache) {
                     console.log('Cache file outdated.');
                     scrape();
                 } else {
@@ -40,16 +39,16 @@ module.exports = {
             }
         });
     },
-    clearAll: function(req,res) {
+    clearAll: function(callback) {
         House.remove({}, function(err) {
             console.log('collection removed');
+            callback();
         });
     },
     matchToModel: function(house) {
         // go through the properties of the house
         for(var z in house) {
-            if(z == 'died out')
-            {
+            if(z == 'died out') {
                 house.isExtinct = true;
             }
 
@@ -72,8 +71,6 @@ module.exports = {
                     house[z] = 0 - Math.abs(house[z].replace(/\D/g, ''));
                 }
                 else {
-                    // TODO: it is the name of an age, which has to be transformed into a date
-
                     delete house[z]; // ignore it for now
                 }
             }
@@ -83,31 +80,68 @@ module.exports = {
     },
     insertToDb: function(houses, callback) {
         console.log('Inserting into db..');
-        var i = 0;
 
-        // iterate through houses
-        async.forEach(houses, function (house, _callback) {
+        var addHouse = function(house, callb) {
+            Houses.add(house, function (success, data) {
+                console.log((success != 1) ? 'Problem:' + data : 'SUCCESS: ' + data.name);
+                callb(true);
+            });
+        }
+
+        var insert = function (houses) {
+            // iterate through houses
+            async.forEach(houses, function (house, _callback) {
                 // name is required
                 if (!house.hasOwnProperty('name')) {
                     _callback();
                     return;
                 }
 
-                var filler = require(__appbase + 'controllers/filler/houses');
-                house = filler.matchToModel(house);
-                // add house to db
-                Houses.add(house, function (success, data) {
-                    if (success != 1) {
-                        console.log('Problem:' + data);
-                    }
-                    else {
-                        console.log('SUCCESS: ' + data.name);
-                    }
-                    _callback();
-                });
+                house = module.exports.matchToModel(house);
+
+                if(cfg.fillerPolicy == 1) { // empty db, so just add it
+                    addHouse(house, function(suc){ _callback(); });
+                }
+                else {
+                    // see if there is such an entry already in the db
+                    Houses.getByName(house.name,function(success,oldHouse){
+                        if(success == 1) { // old entry is existing
+                            if(cfg.fillerPolicy == 3) { // only add new entries, so skip
+                                console.log(house.name + " is already existing. No change according policy.")
+                                _callback(); // next
+                                return;
+                            }
+                            else if(cfg.fillerPolicy == 2) { // update old entry
+                                console.log("Update house: " + house.name)
+                                for(var z in house) {
+                                    if(z != "_id") {
+                                        oldHouse[z] = house[z];
+                                    }
+                                }
+                                oldHouse['updatedAt'] = new Date();
+                                oldHouse.save(function(err){
+                                    _callback();
+                                });
+                            }
+                        }
+                        else { // not existing, so it is added in every policy
+                            addHouse(house, function(suc){_callback();});
+                        }
+                    });
+
+                }
             },
-            function (err) {
-                callback(true);
-            });
+            function (err) { callback(true); }
+            );
+        }
+
+        // delete the collection before the insertion?
+        if(cfg.fillerPolicy == 1) {
+            console.log("Delete and refill policy. Deleting collection..")
+            module.exports.clearAll(function() {insert(houses)});
+        }
+        else {
+            insert(houses);
+        }
     }
 };
