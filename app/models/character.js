dcd302dad1902d9a02028edf835c1170d13232e7
MiLe <michael.legenc@tum.de>
2016-03-18 16:38:02 +0100

    #86 remove parents property

--- app/models/character.js
+++ app/models/character.js
@@ -10,30 +10,22 @@ var CharacterSchema = new Schema({
     dateOfBirth: Number,                                                        // 259 AC
     dateOfDeath: Number,  	                                                     // 283 AC
     actor      : String,
-
     mother: {type: String, ref: 'Character'},
     father: {type: String, ref: 'Character'},
     heir  : {type: String, ref: 'Character'},
-
     placeOfBirth: {type: String, ref: "Region"},                                                 // Summerhall
     placeOfDeath: {type: String, ref: "Region"},                                                 // Trident
     plod: {type: Number},                                         // %-Likelyhood of Death within actual season, e.g. 84%
     house       : {type: String, ref: "House"},                   // House Targaryen
     createdAt   : {type: Date, default: Date.now},
     updatedAt   : {type: Date, default: Date.now},
-
-    //issue #72 fields
     spouse             : {type: String, ref: 'Character'},
     allegiance         : {type: String, ref: 'Character'},
     characterPopularity: Number,
     books              : [String],
     placeOfLastVisit   : {type: String, ref: "Region"},
-    
-    //issue 85 fields
     imageLink			:	String,
     hasPath				:	Boolean
-    
-
 });
 
 module.exports = mongoose.model('Character', CharacterSchema);
