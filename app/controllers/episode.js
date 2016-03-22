697ffcbc722bbadd6829b9ad6e631e5d3316a8f4
Kordian Bruck <k@bruck.me>
2016-03-22 21:30:32 +0100

    #108 acutally remove all methods from the controllers and delete all routes

--- app/controllers/episode.js
+++ app/controllers/episode.js
@@ -1,48 +1,5 @@
 module.exports = {
     /**
-     * @api {post} /api/episodes/ Add an episode
-     * @apiVersion 0.0.1
-     * @apiName AddEpisode
-     * @apiGroup Episodes
-     *
-     * @apiExample {post} Example
-     *     {"name": "The Wars to Come"}
-     *
-     * @apiSuccessExample {json} Success-Response
-     *     HTTP/1.1 200 OK
-     *     {"message" : "Success", "data" : episode}
-     *
-     * @apiError (400) PropertyInvalid A property of the request is not valid to the underlying schema.
-     * @apiErrorExample {json} PropertyInvalid
-     *      HTTP/1.1 400
-     *      {
-     *          "message": "Error. Property not valid to schema.",
-     *          "errorProperty": prop
-     *      }
-     *
-     * @apiError (400) BadRequestError A value for a property is not valid to the underlying schema.
-     * @apiErrorExample {json} BadRequestError
-     *     HTTP/1.1 400
-     *     {"message" : "Error: Bad request", "error" : err}
-     *
-     * @apiDescription Add an episode to the collection with all the required fields.<br><br>
-     * The name property is required.
-     * Check the <a href="https://github.com/Rostlab/JS16_ProjectA/blob/master/app/models/episode.js" target="_blank">episode model</a>.
-     */
-    add: function (req, res) {
-        var episodesStore = require('../stores/episodes');
-        episodesStore.add(req.body, function (success, message) {
-            if (success == 1) {
-                res.status(201).json({message: 'Success', data: message});
-            } else if (success == 2) {
-                res.status(400).json({message: 'Error. Property not valid to schema.', errorProperty: message});
-            } else {
-                res.status(400).json({message: 'Error.', error: message});
-            }
-        });
-    },
-
-    /**
      * @api {get} /api/episodes/ Get all episodes
      * @apiVersion 0.0.1
      * @apiName GetAllEpisode
@@ -168,113 +125,34 @@ module.exports = {
             }
         });
     },
-
     /**
-     * @api {put} /api/episodes/:id Edit episode
+     * @api {get} /api/episodes/byCharacter/:id Get episodes by character
      * @apiVersion 0.0.1
-     * @apiName EditEpisode
+     * @apiName GetEpisodesByCharacter
      * @apiGroup Episodes
      *
-     * @apiExample {post} Example
-     *     {"episodeLength": 65} // set length to 65min
-     *
-     * @apiParam {String} id Episode _id
+     * @apiParam {String} id Character _id
      * @apiSuccessExample {json} Success-Response
      *     HTTP/1.1 200 OK
-     *     {"message" : "Success", "data" : episode}
+     *     {"message" : "Success", "data" : episodes}
      *
-     * @apiError (404) NotFound No episode with that data existing!
+     * @apiError (404) NotFound No episode with that character existing!
      * @apiErrorExample {json} NotFound
      *      HTTP/1.1 404
-     *      { "message": "Error. No episode exsiting with that id", "id": episode._id };
+     *      { "message": "Failure. No episode with that character existing!", "data": err };
      *
-     * @apiError (400) InvalidProperty No such property
-     * @apiErrorExample {json} InvalidProperty
-     *      HTTP/1.1 404
-     *      { "message": "Error: Bad request. No such property", "errorProperty": property };
-     *
-     * @apiError (400) GeneralError Mongoose error.
-     * @apiErrorExample {json} GeneralError
-     *      HTTP/1.1 404
-     *      { "message": "Error", "error": err };
-     *
-     * @apiDescription Update an episode with the id :id with some new information.<br>
-     * Check the <a href="https://github.com/Rostlab/JS16_ProjectA/blob/master/app/models/episode.js" target="_blank">episode model</a>.
+     * @apiDescription Search episodes in which the character id appears.
      */
-    edit: function (req, res) {
-        var episodesStore = require('../stores/episodes');
-
-        episodesStore.edit(req.params.id, req.body, function (success, message) {
-            if (success == 1) {
-                res.status(200).json({message: 'Success', data: message});
-            } else if (success == 2) {
-                res.status(404).json({message: 'Error. No episode exsiting with that id', id: req.params.id});
-            } else if (success == 4) {
-                res.status(400).json({message: 'Error: Bad request. No such property.', errorProperty: message});
-            } else {
-                res.status(400).json({message: 'Error', error: message});
-            }
-        });
-    },
-
-    /**
-     * @api {delete} /api/episodes/:id Remove episode
-     * @apiVersion 0.0.1
-     * @apiName RemoveEpisode
-     * @apiGroup Episodes
-     *
-     * @apiParam {String} id Episode _id
-     * @apiSuccessExample {json} Success-Response
-     *     HTTP/1.1 200 OK
-     *     {"message" : "Success"}
-     *
-     * @apiError (404) NotFound No episode with that data existing!
-     * @apiErrorExample {json} NotFound
-     *      HTTP/1.1 404
-     *      { "message": "Failure. No episode with that data existing!", "data": err };
-     *
-     * @apiDescription Delete the episode with the :id.
-     */
-    remove: function (req, res) {
-        var episodesStore = require('../stores/episodes');
-        episodesStore.remove(req.params.id, function (success) {
-            if (success === true) {
-                res.status(200).json({message: 'Success'});
-            } else {
-                res.status(404).json({message: 'Failure: No episode with that id is existing.', id: req.params.id});
-            }
-        });
-    },
-
-
-     /**
-      * @api {get} /api/episodes/byCharacter/:id Get episodes by character
-      * @apiVersion 0.0.1
-      * @apiName GetEpisodesByCharacter
-      * @apiGroup Episodes
-      *
-      * @apiParam {String} id Character _id
-      * @apiSuccessExample {json} Success-Response
-      *     HTTP/1.1 200 OK
-      *     {"message" : "Success", "data" : episodes}
-      *
-      * @apiError (404) NotFound No episode with that character existing!
-      * @apiErrorExample {json} NotFound
-      *      HTTP/1.1 404
-      *      { "message": "Failure. No episode with that character existing!", "data": err };
-      *
-      * @apiDescription Search episodes in which the character id appears.
-      */
     getEpisodesByCharacter: function (req, res) {
         var episodesStore = require('../stores/episodes');
-        episodesStore.getEpisodesByCharacter(req.params.id , function (success, message) {
+        episodesStore.getEpisodesByCharacter(req.params.id, function (success, message) {
             if (success === 1) {
                 res.status(200).json({message: 'Success', data: obj});
-            }else if (success === 2){
+            } else if (success === 2) {
                 res.status(404).json({message: 'Error', error: message});
-            }else{
-		res.status(404).json({message: 'Failure: No episode with that character is existing.', data: message});
-	    } 
+            } else {
+                res.status(404).json({message: 'Failure: No episode with that character is existing.', data: message});
+            }
         });
     }
 
