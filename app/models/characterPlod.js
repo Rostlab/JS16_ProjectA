cc37cdcad8c9086c56d09a6fbe580f94fd85adc4
Togi Dashnyam <togi.berlin@gmail.com>
2016-03-22 22:51:09 +0100

    Edited characterPlod model, inserted Kordians hints #101

--- app/models/characterPlod.js
+++ app/models/characterPlod.js
@@ -4,9 +4,8 @@ var Schema = mongoose.Schema;
 var CharacterPlodSchema = new Schema({
     character: {type: Schema.Types.ObjectId, ref: 'Character'},
     plod: {type: Number},           //% likelihood of death
-    status: {type: String},			// 'alive' / 'dead'
     date: {type: Date},				// It allows a timeline, if necessary
-    description: {type: String}     // Group B6, Group B7, Method XYZ
+    algorithm: {type: String}     // Group B6, Group B7, Method XYZ
 });
 
 module.exports = mongoose.model('CharacterPlod', CharacterPlodSchema);
