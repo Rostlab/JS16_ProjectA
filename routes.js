8f005453fed1141431723ab1e84dad4d71636fb5
Kordian Bruck <k@bruck.me>
2016-03-22 20:12:34 +0100

    #108 also remove adding to the collections

--- routes.js
+++ routes.js
@@ -4,85 +4,85 @@ module.exports = function (app, router) {
     router.get('/', defController.init);
 
     var housesController = require(__appbase + 'controllers/house');
-    router.post('/houses', housesController.add);
+    //router.post('/houses', housesController.add);
     router.post('/houses/find', housesController.get);
     router.get('/houses', housesController.getAll);
     router.get('/houses/:houseName', housesController.getByName);
     router.get('/houses/byId/:houseId', housesController.getById);
-   // router.delete('/houses/:houseId', housesController.remove);
-   // router.put('/houses/:houseId', housesController.edit);
+    //router.delete('/houses/:houseId', housesController.remove);
+    //router.put('/houses/:houseId', housesController.edit);
 
     var episodeController = require(__appbase + 'controllers/episode');
-    router.post('/episodes', episodeController.add);
+    //router.post('/episodes', episodeController.add);
     router.post('/episodes/find', episodeController.get);
     router.get('/episodes', episodeController.getAll);
     router.get('/episodes/:name', episodeController.getByName);
     router.get('/episodes/byId/:id', episodeController.getById);
     router.get('/episodes/byCharacter/:name', episodeController.getEpisodesByCharacter);
-   // router.delete('/episodes/:id', episodeController.remove);
-   // router.put('/episodes/:id', episodeController.edit);
+    //router.delete('/episodes/:id', episodeController.remove);
+    //router.put('/episodes/:id', episodeController.edit);
 
     var eventsController = require(__appbase + 'controllers/event');
-    router.post('/events', eventsController.add);
+    //router.post('/events', eventsController.add);
     router.post('/events/find', eventsController.get);
     router.get('/events', eventsController.getAll);
     router.get('/events/:name', eventsController.getByName);
     router.get('/events/byId/:id', eventsController.getById);
     router.get('/episodes/byCharacter/:id', episodeController.getEpisodesByCharacter);
-   // router.delete('/events/:id', eventsController.remove);
-   // router.put('/events/:id', eventsController.edit);
+    //router.delete('/events/:id', eventsController.remove);
+    //router.put('/events/:id', eventsController.edit);
 
     var ageController = require(__appbase + 'controllers/age');
-    router.post('/ages', ageController.add);
+    //router.post('/ages', ageController.add);
     router.post('/ages/find', ageController.get);
     router.get('/ages', ageController.getAll);
     router.get('/ages/:name', ageController.getByName);
     router.get('/ages/byId/:id', ageController.getById);
-  //  router.delete('/ages/:id', ageController.remove);
-  //  router.put('/ages/:id', ageController.edit);
+    //router.delete('/ages/:id', ageController.remove);
+    //router.put('/ages/:id', ageController.edit);
 
     var characterController = require(__appbase + 'controllers/character');
-    router.post('/characters', characterController.add);
+    //router.post('/characters', characterController.add);
     router.post('/characters/find', characterController.get);
     router.get('/characters', characterController.getAll);
     router.get('/characters/:name', characterController.getByName);
     router.get('/characters/byId/:id', characterController.getById);
     router.get('/characters/plod/:count', characterController.getByPLOD);
-  //  router.delete('/characters/:id', characterController.remove);
-  //  router.put('/characters/:id', characterController.edit);
+    //router.delete('/characters/:id', characterController.remove);
+    //router.put('/characters/:id', characterController.edit);
 
     var geographyController = require(__appbase + 'controllers/geography');
-    router.post('/continents', geographyController.addContinent);
+    //  router.post('/continents', geographyController.addContinent);
     router.post('/continents/find', geographyController.getContinents);
     router.get('/continents', geographyController.getAllContinents);
     router.get('/continents/:name', geographyController.getContinentByName);
     router.get('/continents/byId/:id', geographyController.getContinentById);
-   // router.delete('/continents/:id', geographyController.removeContinent);
-   // router.put('/continents/:id', geographyController.editContinent);
+    //router.delete('/continents/:id', geographyController.removeContinent);
+    //router.put('/continents/:id', geographyController.editContinent);
 
-    router.post('/cultures', geographyController.addCulture);
+    //router.post('/cultures', geographyController.addCulture);
     router.post('/cultures/find', geographyController.getCultures);
     router.get('/cultures', geographyController.getAllCultures);
     router.get('/cultures/:name', geographyController.getCultureByName);
     router.get('/cultures/byId/:id', geographyController.getCultureById);
-   // router.delete('/cultures/:id', geographyController.removeCulture);
-   // router.put('/cultures/:id', geographyController.editCulture);
+    //router.delete('/cultures/:id', geographyController.removeCulture);
+    //router.put('/cultures/:id', geographyController.editCulture);
 
-    router.post('/regions', geographyController.addRegion);
+    //router.post('/regions', geographyController.addRegion);
     router.post('/regions/find', geographyController.getRegions);
     router.get('/regions', geographyController.getAllRegions);
     router.get('/regions/:name', geographyController.getRegionByName);
     router.get('/regions/byId/:id', geographyController.getRegionById);
-   // router.delete('/regions/:id', geographyController.removeRegion);
-   // router.put('/regions/:id', geographyController.editRegion);
+    //router.delete('/regions/:id', geographyController.removeRegion);
+    //router.put('/regions/:id', geographyController.editRegion);
 
-    router.post('/cities', geographyController.addCity);
+    //router.post('/cities', geographyController.addCity);
     router.post('/cities/find', geographyController.getCities);
     router.get('/cities', geographyController.getAllCities);
     router.get('/cities/:name', geographyController.getCityByName);
     router.get('/cities/byId/:id', geographyController.getCityById);
-   // router.delete('/cities/:id', geographyController.removeCity);
-   // router.put('/cities/:id', geographyController.editCity);
+    //router.delete('/cities/:id', geographyController.removeCity);
+    //router.put('/cities/:id', geographyController.editCity);
 
     var statsController = require(__appbase + 'controllers/statistics');
     router.get('/stats/', statsController.getStats);
