5ad5b0d4e352543cb6360d175df47ae1644cc3b5
Togi Dashnyam <togi.berlin@gmail.com>
2016-04-27 22:23:13 +0200

    #146 Minor changes, added Date.now as default value for createdAt

--- app/models/characterNews.js
+++ app/models/characterNews.js
@@ -1,11 +1,11 @@
 var mongoose = require('mongoose');
 var Schema   = mongoose.Schema;
 
-var CharacterNotes = new Schema({
+var CharacterNews = new Schema({
     name : {type: String, ref: 'Character', required: true, unique: true},
     slug: {type: String, unique: true, required: true},
     text: {type: String},
-    createdAt: {type: Date}
+    createdAt: {type: Date, required: true, default: Date.now}
 });
 
-module.exports = mongoose.model('CharacterNotes', CharacterNotes);
+module.exports = mongoose.model('CharacterNews', CharacterNews);
